94e1f6b9083e0baadfb6318915bc6f792b544593
Ulysses LI <ulysses142@gmail.com>
2024-01-02 17:32:19 -0500

    refined styles in my header element

--- src/components/UserAvatar/UserAvatar.js
+++ src/components/UserAvatar/UserAvatar.js
@@ -1,8 +1,10 @@
 import "./UserAvatar.scss";
+import defaultAvatar from "../../assets/images/Mohan-muruge.jpg";
 
 const UserAvatar = () => { 
+    console.log(defaultAvatar);
     return (
-        <img src={`../../assets/images/Mohan-muruge.jpg`} alt={"user profile"} />
+        <img class="header__avatar" src={defaultAvatar} alt={"user profile"} />
     );
 };
 
